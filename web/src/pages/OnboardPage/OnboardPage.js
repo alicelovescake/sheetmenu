885a3d21a13a98349c11e166fd21fb10dde87841
Anthony Morris <amorriscode@gmail.com>
2021-04-12 22:36:46 -0700

    Add some styling to the onboarding form

--- web/src/pages/OnboardPage/OnboardPage.js
+++ web/src/pages/OnboardPage/OnboardPage.js
@@ -1,4 +1,4 @@
-import { Link, routes } from '@redwoodjs/router'
+import { navigate, routes } from '@redwoodjs/router'
 import { Form, Label, TextField, FieldError, Submit } from '@redwoodjs/forms'
 
 const OnboardPage = () => {
@@ -7,27 +7,51 @@ const OnboardPage = () => {
   }
 
   return (
-    <>
-      <Form onSubmit={onSubmit}>
-        <Label name="name" className="label" />
-        <TextField
-          name="name"
-          className="input"
-          validation={{ required: true }}
-        />
-        <FieldError name="name" className="error-message" />
+    <main className="max-w-2xl mx-auto p-6">
+      <div className="mb-4 space-y-4 p-6">
+        <h1 className="font-bold text-2xl">Welcome!</h1>
 
-        <Label name="restaurant name" className="label" />
-        <TextField
-          name="restaurant name"
-          className="input"
-          validation={{ required: true }}
-        />
-        <FieldError name="restaurant name" className="error-message" />
+        <p>
+          Thanks for joining sheetmenu! We&apos;ll help you get a website up and
+          running in under five minutes.
+        </p>
 
-        <Submit className="button">Save</Submit>
+        <p>
+          First, we just need a little bit of info about you and your
+          restaurant.
+        </p>
+      </div>
+
+      <Form onSubmit={onSubmit} className="bg-white p-6 rounded-lg space-y-4">
+        <div>
+          <Label name="name" className="font-bold pr-2 text-sm">
+            Name
+          </Label>
+          <TextField
+            name="name"
+            className="bg-gray-100 p-2 rounded-lg block w-full"
+            validation={{ required: true }}
+          />
+          <FieldError name="name" className="error-message" />
+        </div>
+
+        <div>
+          <Label name="restaurantName" className="font-bold pr-2 text-sm">
+            Restaurant Name
+          </Label>
+          <TextField
+            name="restaurantName"
+            className="bg-gray-100 p-2 rounded-lg block w-full"
+            validation={{ required: true }}
+          />
+          <FieldError name="restaurantName" className="error-message" />
+        </div>
+
+        <Submit className="bg-green-800 py-2 px-6 text-white rounded-lg hover:opacity-75">
+          Save
+        </Submit>
       </Form>
-    </>
+    </main>
   )
 }
 
